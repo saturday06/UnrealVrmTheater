a23e4d311051fe360fd9d6754817cd1ddc73d7b6
Isamu Mogi <isamu@leafytree.jp>
2026-01-05 20:22:21 +0900

    Unreal Engine 5.7対応

--- Source/UnrealVrmTheater.Target.cs
+++ Source/UnrealVrmTheater.Target.cs
@@ -6,8 +6,8 @@ public class UnrealVrmTheaterTarget : TargetRules
 	public UnrealVrmTheaterTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("UnrealVrmTheater");
 	}
 }
--- Source/UnrealVrmTheaterEditor.Target.cs
+++ Source/UnrealVrmTheaterEditor.Target.cs
@@ -6,8 +6,8 @@ public class UnrealVrmTheaterEditorTarget : TargetRules
 	public UnrealVrmTheaterEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("UnrealVrmTheater");
 	}
 }
